7096c55484637c7d9f6dcbeb52172f37924676b1
prajjal <prajjaldhar41@gmail.com>
2024-02-08 12:24:25 +0530

    apifetching

---
+++ github_details/src/Components/CardComponent/CardComponent.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import "./CardComponent.css";
+
+const CardComponent = () => {
+  const [datas, setdatas] = useState([]);
+  //fetch data
+  let username = "khemchandsaini365";
+  useEffect(() => {
+    const fetchdata = async () => {
+      try {
+        const res = await fetch(`https://api.github.com/users/${username}`);
+        console.log(res);
+        if (!res.ok) {
+          throw new Error(res.status);
+        }
+        console.log(res);
+        const aakar = await res.json();
+        console.log(aakar);
+        setdatas(aakar);
+        console.log(datas);
+      } catch (err) {
+        console.log(`Error: ${err}(internal server error)`);
+      }
+    };
+    fetchdata();
+  }, []);
+
+  return (
+    <div className="card-container">
+      <div className="cards">
+        <div className="cover-photo">
+          <img src={datas.avatar_url} className="profile" />
+        </div>
+        <h3 className="profile-name">{datas.name}</h3>
+        <p className="about">{datas.bio}</p>
+        <button className="btn">Followers: {datas.followers}</button>
+        <button className="btn">Following: {datas.following}</button>
+        <div className="icons">
+          <i className="fa-brands fa-linkedin" />
+          <i className="fa-brands fa-github" />
+          <i className="fa-brands fa-youtube" />
+          <i className="fa-brands fa-twitter" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardComponent;
---
+++ github_details/src/utils/datas.js
@@ -0,0 +1,38 @@
+const datas = {
+  login: "prajjaldhar",
+  id: 64453189,
+  node_id: "MDQ6VXNlcjY0NDUzMTg5",
+  avatar_url: "https://avatars.githubusercontent.com/u/64453189?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/prajjaldhar",
+  html_url: "https://github.com/prajjaldhar",
+  followers_url: "https://api.github.com/users/prajjaldhar/followers",
+  following_url:
+    "https://api.github.com/users/prajjaldhar/following{/other_user}",
+  gists_url: "https://api.github.com/users/prajjaldhar/gists{/gist_id}",
+  starred_url:
+    "https://api.github.com/users/prajjaldhar/starred{/owner}{/repo}",
+  subscriptions_url: "https://api.github.com/users/prajjaldhar/subscriptions",
+  organizations_url: "https://api.github.com/users/prajjaldhar/orgs",
+  repos_url: "https://api.github.com/users/prajjaldhar/repos",
+  events_url: "https://api.github.com/users/prajjaldhar/events{/privacy}",
+  received_events_url:
+    "https://api.github.com/users/prajjaldhar/received_events",
+  type: "User",
+  site_admin: false,
+  name: "Prj_Rocks",
+  company: null,
+  blog: "https://www.linkedin.com/in/prajjal-dhar-24x7/",
+  location: null,
+  email: null,
+  hireable: true,
+  bio: "MERN STACK DEVELOPER | | SOFTWARE DEVELOPER | | WEB DEVELOPER | | MACHINE LEARNING ENTHUSIAST | | REACT_JS NODEJ_S DEVELOPER | | DATA SCIENTIST",
+  twitter_username: null,
+  public_repos: 45,
+  public_gists: 0,
+  followers: 19,
+  following: 3,
+  created_at: "2020-04-28T03:27:46Z",
+  updated_at: "2024-01-11T14:09:38Z",
+};
+export default datas;
